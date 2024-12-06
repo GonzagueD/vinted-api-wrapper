f5b0bd47b264a26da6f285c6e11ee3ba3ce01ed4
GonzagueD <49749926+GonzagueD@users.noreply.github.com>
2024-12-06 11:23:17 +0100

    Update vinted.py

--- src/pyVinted/vinted.py
+++ src/pyVinted/vinted.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse, parse_qsl
 import requests
-from .items.item import Item
+
 from pyVinted.items import Items
 from pyVinted.requester import requester
 
@@ -8,18 +8,15 @@ class Vinted:
     """
     This class is built to connect with the pyVinted API.
 
-    It's main goal is to be able to retrieve items from a given url search.\n
-
+    It's main goal is to be able to retrieve items from a given url search.
     """
 
-    def __init__(self, proxy=None):
+    def __init__(self, proxies=None):
         """
         Args:
-            Proxy : proxy to be used to bypass vinted's limite rate
-
+            proxies (list[str]): liste de proxies à utiliser, par ex. ["http://user:pass@host:port", "http://..."]
         """
-
-        if proxy is not None:
-            requester.session.proxies.update(proxy)
+        if proxies is not None:
+            requester.set_proxies(proxies)
 
         self.items = Items()
